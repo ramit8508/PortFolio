76a18f69eb702a9a515730897584653badcc6850
Ramit Goyal <ramitgoyal1987@gmail.com>
2025-11-07 16:01:44 +0530

    heading done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './sections/navbar.jsx'
+import Hero from './sections/Hero.jsx'
 
 function App() {
  
 
   return (
-    <>
+    <main className='max-w-7xl mx-auto'>
       <Navbar />
-    </>
+      <Hero />
+    </main>
   )
 }
 
