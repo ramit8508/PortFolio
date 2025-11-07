79f79b395990990e4133ded2eda9a9204f5cc271
Ramit Goyal <ramitgoyal1987@gmail.com>
2025-11-07 22:46:53 +0530

    Updated portfolio with custom projects, certifications, tech stack, and compressed videos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './sections/navbar.jsx'
-import Hero from './sections/Hero.jsx'
+import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
+import Footer from './sections/Footer.jsx';
+import Navbar from './sections/Navbar.jsx';
+import Contact from './sections/Contact.jsx';
+import Clients from './sections/Clients.jsx';
+import Projects from './sections/Projects.jsx';
+import WorkExperience from './sections/Experience.jsx';
 
-function App() {
- 
-
+const App = () => {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <WorkExperience />
+      <Contact />
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
