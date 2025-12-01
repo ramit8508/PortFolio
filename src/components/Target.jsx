39dbed80bd7e1176f70997494eb7bf5c83450726
Ramit Goyal <ramitgoyal1987@gmail.com>
2025-12-01 11:08:12 +0530

    repaired portfolio

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,13 +1,9 @@
-import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
@@ -20,7 +16,39 @@ const Target = (props) => {
 
   return (
     <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
+      <group>
+        {/* Target rings */}
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <torusGeometry args={[1.2, 0.05, 16, 100]} />
+          <meshStandardMaterial color="#ff0000" />
+        </mesh>
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <torusGeometry args={[0.9, 0.05, 16, 100]} />
+          <meshStandardMaterial color="#ffffff" />
+        </mesh>
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <torusGeometry args={[0.6, 0.05, 16, 100]} />
+          <meshStandardMaterial color="#ff0000" />
+        </mesh>
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <torusGeometry args={[0.3, 0.05, 16, 100]} />
+          <meshStandardMaterial color="#ffffff" />
+        </mesh>
+        {/* Center */}
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <circleGeometry args={[0.15, 32]} />
+          <meshStandardMaterial color="#ff0000" />
+        </mesh>
+        {/* Stand */}
+        <mesh position={[0, -0.75, 0]}>
+          <cylinderGeometry args={[0.05, 0.05, 1.5, 16]} />
+          <meshStandardMaterial color="#333333" />
+        </mesh>
+        <mesh position={[0, -1.5, 0]}>
+          <cylinderGeometry args={[0.3, 0.3, 0.1, 16]} />
+          <meshStandardMaterial color="#333333" />
+        </mesh>
+      </group>
     </mesh>
   );
 };
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
               <p className="grid-headtext">Hi, I’m Ramit Goyal</p>
@@ -36,7 +36,7 @@ const About = () => {
 
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
               <p className="grid-headtext">Tech Stack</p>
@@ -112,7 +112,7 @@ const About = () => {
 
         <div className="xl:col-span-2 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+            <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
 
             <div>
               <p className="grid-headtext">My Passion for Coding</p>
@@ -127,7 +127,7 @@ const About = () => {
         <div className="xl:col-span-1 xl:row-span-2">
           <div className="grid-container">
             <img
-              src="assets/grid4.png"
+              src="/assets/grid4.png"
               alt="grid-4"
               className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
             />
@@ -135,7 +135,7 @@ const About = () => {
             <div className="space-y-2">
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
-                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
                 <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">ramitgoyal1987@gmail.com</p>
               </div>
             </div>
